c8fdacbc9c2f82bf7fb4bf8b9f3f6ffc27f2abf6
Amazon2011 <yongtao.chen@capstonegaming.com>
2017-04-12 21:09:40 +0800

    Update

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,9 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include "Sales_item.h"
+#include "Sales_data.h"
 
 int main()
 {
@@ -96,10 +98,33 @@ int main()
     /*int i2 = 1;
     const int *const p2 = &i2;
     const int *p1 = &i2;*/
-    auto i = 0;
+    /*auto i = 0;
     auto j = 1;
 
-    std::cout << "i + j = " << i + j << std::endl;
+    std::cout << "i + j = " << i + j << std::endl;*/
+    Sales_data data1, data2;
+    double price = 0;
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    data1.revenue = data1.units_sold * price;
+
+    std::cin >> data2.bookNo >> data2.units_sold >> price;
+    data2.revenue = data2.units_sold * price;
+
+    if (data1.bookNo == data2.bookNo) {
+        unsigned totalCnt = data1.units_sold + data2.units_sold;
+        double totalRevenue = data1.revenue + data2.revenue;
+        std::cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+        if (totalCnt != 0)
+            std::cout << totalRevenue / totalCnt << std::endl;
+        else
+            std::cout << "(no sales)" << std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Data must refer to the same ISBN" << std::endl;
+        return -1;
+    }
 
     return 0;
 }
